1bca3d1de63170fed81adf80fde81395827fccaa
AGretskikh <gretskih@mail.ru>
2023-10-26 14:52:49 +0300

    Тестирование с библиотекой Mockito.

--- src/test/test/ru/job4j/tracker/StartUITest.java
+++ src/test/test/ru/job4j/tracker/StartUITest.java
@@ -9,12 +9,16 @@ import ru.job4j.tracker.output.ConsoleOutput;
 import ru.job4j.tracker.output.Output;
 import ru.job4j.tracker.output.StubOutput;
 import ru.job4j.tracker.store.MemTracker;
+import ru.job4j.tracker.store.Store;
 
 import java.util.Arrays;
 import java.util.Objects;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static org.hamcrest.core.IsNull.nullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class StartUITest {
 
@@ -172,5 +176,117 @@ public class StartUITest {
                 + is + "0. === Find item by id ===" + is
                 + "1. Exit" + is + "Founded item: " + item
                 + is + "Menu." + is + "0. === Find item by id ===" + is + "1. Exit" + is));
+    }
+
+    @Test
+    public void whenDeleteItemMock() {
+        Output output = new StubOutput();
+        Store store = new MemTracker();
+        store.add(new Item("Deleted item"));
+        DeleteAction del = new DeleteAction(output);
+        String delId = "1";
+
+        Input input = mock(Input.class);
+
+        when(input.askStr(any(String.class))).thenReturn(delId);
+        del.execute(input, store);
+
+        String ln = System.lineSeparator();
+        assertThat(output.toString(), is("Item is successfully deleted!" + ln));
+        assertThat(store.findAll().isEmpty(), is(true));
+    }
+
+    @Test
+    public void whenDeleteItemMockFail() {
+        Output output = new StubOutput();
+        Store store = new MemTracker();
+        store.add(new Item("Deleted item"));
+        DeleteAction del = new DeleteAction(output);
+        String delId = "2";
+
+        Input input = mock(Input.class);
+
+        when(input.askStr(any(String.class))).thenReturn(delId);
+        del.execute(input, store);
+
+        String ln = System.lineSeparator();
+        assertThat(output.toString(), is("Wrong id!" + ln));
+        assertThat(store.findAll().isEmpty(), is(false));
+    }
+
+    @Test
+    public void findByIdActionMock() {
+        Output output = new StubOutput();
+        Store store = new MemTracker();
+        Item addItem = new Item("Found item");
+        store.add(addItem);
+        FindByIdAction find = new FindByIdAction(output);
+        String findId = "1";
+
+        Input input = mock(Input.class);
+
+        when(input.askStr(any(String.class))).thenReturn(findId);
+        find.execute(input, store);
+
+        String ln = System.lineSeparator();
+        assertThat(output.toString(), is("Founded item: " + addItem + ln));
+        assertThat(store.findAll().get(0).getName(), is(addItem.getName()));
+    }
+
+    @Test
+    public void findByIdActionMockFail() {
+        Output output = new StubOutput();
+        Store store = new MemTracker();
+        Item addItem = new Item("Found item");
+        store.add(addItem);
+        FindByIdAction find = new FindByIdAction(output);
+        String findId = "2";
+
+        Input input = mock(Input.class);
+
+        when(input.askStr(any(String.class))).thenReturn(findId);
+        find.execute(input, store);
+
+        String ln = System.lineSeparator();
+        assertThat(output.toString(), is("Wrong id! Not found" + ln));
+        assertThat(store.findAll().get(0).getName(), is(addItem.getName()));
+    }
+
+    @Test
+    public void findByNameActionMock() {
+        Output output = new StubOutput();
+        Store store = new MemTracker();
+        String findName = "Found item";
+        Item addItem = new Item(findName);
+        store.add(addItem);
+        FindByNameAction find = new FindByNameAction(output);
+
+        Input input = mock(Input.class);
+
+        when(input.askStr(any(String.class))).thenReturn(findName);
+        find.execute(input, store);
+
+        String ln = System.lineSeparator();
+        assertThat(output.toString(), is(addItem.toString() + ln));
+        assertThat(store.findAll().get(0).getName(), is(findName));
+    }
+
+    @Test
+    public void findByNameActionMockFail() {
+        Output output = new StubOutput();
+        Store store = new MemTracker();
+        String findName = "Found item";
+        Item addItem = new Item(findName);
+        store.add(addItem);
+        FindByNameAction find = new FindByNameAction(output);
+
+        Input input = mock(Input.class);
+
+        when(input.askStr(any(String.class))).thenReturn("New item");
+        find.execute(input, store);
+
+        String ln = System.lineSeparator();
+        assertThat(output.toString(), is("Error, we can`t find item" + ln));
+        assertThat(store.findAll().get(0).getName(), is(findName));
     }
 }
